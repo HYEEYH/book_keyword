85ab7373aced3397a37c8f1e5e9716e1c0732639
HYEEYH <hye890@daum.net>
2023-06-08 15:09:13 +0900

    20230608 1.0 디버깅 - 차트 한글 깨짐

--- app_age.py
+++ app_age.py
@@ -15,6 +15,12 @@ from matplotlib import font_manager, rc
 from matplotlib import rcParams
 
 from PIL import Image
+
+import platform
+from matplotlib import font_manager, rc
+plt.rcParams['axes.unicode_minus'] = False
+if platform.system() == 'Linux':
+    rc('font', family='NanumGothic')
 # -----------------------------------
 
 
